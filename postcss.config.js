b54828f53f1eda53cc464530acdc23734c4572ec
Andreyribeiro392 <andreyribeiro392@gmail.com>
2026-06-11 19:47:21 -0300

    fix postcss merge conflict

--- postcss.config.js
+++ postcss.config.js
@@ -1,19 +1,6 @@
 export default {
   plugins: {
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 7134618968fe316e66e28c16a3aa9e91197bd436
-    tailwindcss: {},
-    autoprefixer: {}
-  }
-}
-<<<<<<< HEAD
-=======
-=======
     "@tailwindcss/postcss": {},
     autoprefixer: {},
   },
 };
->>>>>>> e2566c5acaec9c358505328a126895ec33129987
->>>>>>> 7134618968fe316e66e28c16a3aa9e91197bd436
